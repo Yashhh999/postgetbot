468f6622214a90491237786b578b5a5eb36413ce
Yash Gupta <gupta25yash@gmail.com>
2024-08-11 22:18:52 +0530

    .env

--- Config/bot.js
+++ Config/bot.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   TOKEN:
-    process.env.TOKEN || "", // Add your token in .env file or just add in String
+    process.env.TOKEN || "", 
   PREFIX: process.env.PREFIX || ",",
   Slash: {
     Global: true, // set false for loading slash command in your testing guild make sure you add your server id
